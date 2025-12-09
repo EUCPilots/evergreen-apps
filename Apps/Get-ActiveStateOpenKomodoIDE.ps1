afb4b1f110f4869f286506333d4de1f04e29abb8
Aaron Parker <aaron@stealthpuppy.com>
2025-12-09 14:47:58 +1100

    Add/update GitHub apps

---
+++ Apps/Get-ActiveStateOpenKomodoIDE.ps1
@@ -0,0 +1,26 @@
+function Get-ActiveStateOpenKomodoIDE {
+    <#
+        .SYNOPSIS
+            Returns the latest ActiveState Software ActiveState Komodo Edit version number and download.
+
+        .NOTES
+            Author: Aaron Parker
+    #>
+    [OutputType([System.Management.Automation.PSObject])]
+    [CmdletBinding(SupportsShouldProcess = $false)]
+    param (
+        [Parameter(Mandatory = $false, Position = 0)]
+        [ValidateNotNull()]
+        [System.Management.Automation.PSObject]
+        $res = (Get-FunctionResource -AppName ("$($MyInvocation.MyCommand)".Split("-"))[1])
+    )
+
+    # Pass the repo releases API URL and return a formatted object
+    $params = @{
+        Uri          = $res.Get.Uri
+        MatchVersion = $res.Get.MatchVersion
+        Filter       = $res.Get.MatchFileTypes
+    }
+    $object = Get-GitHubRepoRelease @params
+    Write-Output -InputObject $object
+}
---
+++ Apps/Get-RobCaelersWorkrave.ps1
@@ -0,0 +1,27 @@
+function Get-RobCaelersWorkrave {
+    <#
+        .SYNOPSIS
+            Returns the latest Rob Caelers Workrave version number and download.
+
+        .NOTES
+            Author: Aaron Parker
+    #>
+    [OutputType([System.Management.Automation.PSObject])]
+    [CmdletBinding(SupportsShouldProcess = $false)]
+    param (
+        [Parameter(Mandatory = $false, Position = 0)]
+        [ValidateNotNull()]
+        [System.Management.Automation.PSObject]
+        $res = (Get-FunctionResource -AppName ("$($MyInvocation.MyCommand)".Split("-"))[1])
+    )
+
+    # Pass the repo releases API URL and return a formatted object
+    $params = @{
+        Uri          = $res.Get.Uri
+        MatchVersion = $res.Get.MatchVersion
+        VersionTag   = "name"
+        Filter       = $res.Get.MatchFileTypes
+    }
+    $object = Get-GitHubRepoRelease @params
+    Write-Output -InputObject $object
+}
