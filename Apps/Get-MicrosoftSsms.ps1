9ba19de4ab55e305a8632dec1a512a636a737fa5
Aaron Parker <aaron@stealthpuppy.com>
2026-01-26 08:18:48 +1100

    Simplify SSMS download logic and update manifest
    Refactored Get-MicrosoftSsms.ps1 to use a direct download URL and simplified output object, removing language-specific logic and XML parsing. Updated MicrosoftSsms.json manifest to use a static download URL, version, and new release notes source.

--- Apps/Get-MicrosoftSsms.ps1
+++ Apps/Get-MicrosoftSsms.ps1
@@ -17,29 +17,15 @@ function Get-MicrosoftSsms {
         $res = (Get-FunctionResource -AppName ("$($MyInvocation.MyCommand)".Split("-"))[1])
     )
 
-    # Follow the https://go.microsoft.com/fwlink/?linkid= link to get to the update XML
-    $UpdateFeed = Invoke-EvergreenRestMethod -Uri $res.Get.Update.Uri
-    if ($UpdateFeed -is [System.Xml.XmlElement]) {
-
-        foreach ($Entry in $UpdateFeed) {
-            foreach ($language in $res.Get.Download.Language.GetEnumerator()) {
-
-                # Follow the download link which will return a 301
-                $Query = "?clcid="
-                $Uri = "$($Entry.link.href)$($Query)$($res.Get.Download.Language[$language.key])"
-                $ResponseUri = Resolve-SystemNetWebRequest -Uri $Uri
-                if ($ResponseUri.ResponseUri -is [System.Uri]) {
-
-                    # Construct the output; Return the custom object to the pipeline
-                    $PSObject = [PSCustomObject] @{
-                        Version  = $Entry.component.version
-                        Date     = ConvertTo-DateTime -DateTime ($UpdateFeed.updated.Split(".")[0]) -Pattern $res.Get.Update.DatePattern
-                        Language = $language.key
-                        URI      = $ResponseUri.ResponseUri.AbsoluteUri
-                    }
-                    Write-Output -InputObject $PSObject
-                }
-            }
+    $ResolvedUrl = Resolve-SystemNetWebRequest -Uri $res.Get.Download.Uri
+    if ($ResolvedUrl) {
+        # Construct the output; Return the custom object to the pipeline
+        $PSObject = [PSCustomObject] @{
+            Version = $res.Get.Download.Version
+            Date    = $ResolvedUrl.LastModified.ToShortDateString()
+            Type    = Get-FileType -File $ResolvedUrl.ResponseUri.AbsoluteUri
+            URI     = $ResolvedUrl.ResponseUri.AbsoluteUri
         }
+        Write-Output -InputObject $PSObject
     }
 }
