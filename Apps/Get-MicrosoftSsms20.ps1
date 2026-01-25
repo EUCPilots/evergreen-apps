96449960b192555c0333d40493ce644adde66fdf
Aaron Parker <aaron@stealthpuppy.com>
2026-01-26 08:49:30 +1100

    Add manifests and scripts for SSMS 20 and 21
    Added new PowerShell scripts and manifest files for Microsoft SQL Server Management Studio versions 20 and 21. Updated the existing manifest to specify SSMS 22. These changes enable support for managing and retrieving information for multiple SSMS versions.

---
+++ Apps/Get-MicrosoftSsms20.ps1
@@ -0,0 +1,40 @@
+function Get-MicrosoftSsms20 {
+    <#
+        .SYNOPSIS
+            Returns the latest SQL Server Management Studio
+
+        .NOTES
+            Author: Aaron Parker
+
+    #>
+    [OutputType([System.Management.Automation.PSObject])]
+    [Diagnostics.CodeAnalysis.SuppressMessageAttribute("PSUseSingularNouns", "", Justification = "Product name is a plural")]
+    [CmdletBinding(SupportsShouldProcess = $false)]
+    param (
+        [Parameter(Mandatory = $false, Position = 0)]
+        [ValidateNotNull()]
+        [System.Management.Automation.PSObject]
+        $res = (Get-FunctionResource -AppName ("$($MyInvocation.MyCommand)".Split("-"))[1])
+    )
+
+    foreach ($language in $res.Get.Download.Language.GetEnumerator()) {
+        # Follow the download link which will return a 301
+        $Query = "&clcid="
+        $Uri = "$($res.Get.Download.Uri)$($Query)$($res.Get.Download.Language[$language.key])"
+        Write-Verbose -Message "$($MyInvocation.MyCommand): Resolving: $Uri"
+        $ResponseUri = Resolve-SystemNetWebRequest -Uri $Uri
+
+        if ($ResponseUri.ResponseUri -is [System.Uri]) {
+
+            # Construct the output; Return the custom object to the pipeline
+            $PSObject = [PSCustomObject] @{
+                Version  = $res.Get.Download.Version
+                Date     = $ResponseUri.LastModified.ToShortDateString()
+                Language = $language.key
+                Type     = Get-FileType -File $ResolvedUrl.ResponseUri.AbsoluteUri
+                URI      = $ResponseUri.ResponseUri.AbsoluteUri
+            }
+            Write-Output -InputObject $PSObject
+        }
+    }
+}
---
+++ Apps/Get-MicrosoftSsms21.ps1
@@ -0,0 +1,31 @@
+function Get-MicrosoftSsms21 {
+    <#
+        .SYNOPSIS
+            Returns the latest SQL Server Management Studio
+
+        .NOTES
+            Author: Aaron Parker
+
+    #>
+    [OutputType([System.Management.Automation.PSObject])]
+    [Diagnostics.CodeAnalysis.SuppressMessageAttribute("PSUseSingularNouns", "", Justification = "Product name is a plural")]
+    [CmdletBinding(SupportsShouldProcess = $false)]
+    param (
+        [Parameter(Mandatory = $false, Position = 0)]
+        [ValidateNotNull()]
+        [System.Management.Automation.PSObject]
+        $res = (Get-FunctionResource -AppName ("$($MyInvocation.MyCommand)".Split("-"))[1])
+    )
+
+    $ResolvedUrl = Resolve-SystemNetWebRequest -Uri $res.Get.Download.Uri
+    if ($ResolvedUrl) {
+        # Construct the output; Return the custom object to the pipeline
+        $PSObject = [PSCustomObject] @{
+            Version = $res.Get.Download.Version
+            Date    = $ResolvedUrl.LastModified.ToShortDateString()
+            Type    = Get-FileType -File $ResolvedUrl.ResponseUri.AbsoluteUri
+            URI     = $ResolvedUrl.ResponseUri.AbsoluteUri
+        }
+        Write-Output -InputObject $PSObject
+    }
+}
