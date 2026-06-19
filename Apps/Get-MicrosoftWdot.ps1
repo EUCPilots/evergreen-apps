e00bb56ed9b515a14973466b5466e16e86eddaf9
Aaron Parker <aaron@stealthpuppy.com>
2026-06-19 11:05:35 +1000

    Add MicrosoftWdot
    Add `MicrosoftWdot` -  The-Virtual-Desktop-Team/Windows-Desktop-Optimization-Tool

---
+++ Apps/Get-MicrosoftWdot.ps1
@@ -0,0 +1,36 @@
+function Get-MicrosoftWdot {
+    <#
+        .NOTES
+            Author: Aaron Parker
+
+    #>
+    [OutputType([System.Management.Automation.PSObject])]
+    [Diagnostics.CodeAnalysis.SuppressMessageAttribute("PSUseSingularNouns", "", Justification = "Product name is a plural")]
+    [CmdletBinding(SupportsShouldProcess = $false)]
+    param (
+        [Parameter(Mandatory = $false, Position = 0)]
+        [ValidateNotNull()]
+        [System.Management.Automation.PSObject]
+        $res = (Get-FunctionResource -AppName ("$($MyInvocation.MyCommand)".Split("-"))[1])
+    )
+
+    # Pass the repo releases API URL and return a formatted object
+    $params = @{
+        Uri               = $res.Get.Update.Uri
+        MatchVersion      = $res.Get.Update.MatchVersion
+        Filter            = $res.Get.Update.MatchFileTypes
+        ReturnVersionOnly = $true
+    }
+    $object = Get-GitHubRepoRelease @params
+
+    # Build the output object
+    if ($null -ne $object) {
+        $PSObject = [PSCustomObject] @{
+            Version  = $object.Version
+            Type     = Get-FileType -File $res.Get.Download.File
+            Filename = $res.Get.Download.File -replace $res.Get.Download.ReplaceText, $object.Version
+            URI      = $res.Get.Download.Uri
+        }
+        Write-Output -InputObject $PSObject
+    }
+}
