b0f059f33d99a9646e4e4be45dff1b32f9610e6f
Aaron Parker <aaron@stealthpuppy.com>
2026-08-04 22:52:00 +1000

    Simplify FSLogix download to single URI
    Replace the Uri dictionary (keyed by channel) with a single URI string and explicit Channel field in the manifest. Update the script accordingly to remove the foreach loop and resolve the single download link directly.

--- Apps/Get-MicrosoftFSLogixApps.ps1
+++ Apps/Get-MicrosoftFSLogixApps.ps1
@@ -8,7 +8,7 @@ function Get-MicrosoftFSLogixApps {
             Author: Aaron Parker
     #>
     [OutputType([System.Management.Automation.PSObject])]
-    [Diagnostics.CodeAnalysis.SuppressMessageAttribute("PSUseSingularNouns", "", Justification="Product name is a plural")]
+    [Diagnostics.CodeAnalysis.SuppressMessageAttribute("PSUseSingularNouns", "", Justification = "Product name is a plural")]
     [CmdletBinding(SupportsShouldProcess = $false)]
     param (
         [Parameter(Mandatory = $false, Position = 0)]
@@ -17,22 +17,19 @@ function Get-MicrosoftFSLogixApps {
         $res = (Get-FunctionResource -AppName ("$($MyInvocation.MyCommand)".Split("-"))[1])
     )
 
-    foreach ($item in $res.Get.Download.Uri.GetEnumerator()) {
+    # Follow the download link which will return a 301
+    $response = Resolve-SystemNetWebRequest -Uri $res.Get.Download.Uri
 
-        # Follow the download link which will return a 301
-        $response = Resolve-SystemNetWebRequest -Uri $res.Get.Download.Uri[$item.Key]
+    # Check returned URL. It should be a go.microsoft.com/fwlink/?linkid style link
+    if ($null -ne $response) {
 
-        # Check returned URL. It should be a go.microsoft.com/fwlink/?linkid style link
-        if ($null -ne $response) {
-
-            # Construct the output; Return the custom object to the pipeline
-            $PSObject = [PSCustomObject] @{
-                Version = [RegEx]::Match($($response.ResponseUri.AbsoluteUri), $res.Get.Download.MatchVersion).Captures.Value
-                Date    = ConvertTo-DateTime -DateTime $response.LastModified -Pattern $res.Get.Download.DatePattern
-                Channel = $item.Name
-                URI     = $response.ResponseUri.AbsoluteUri
-            }
-            Write-Output -InputObject $PSObject
+        # Construct the output; Return the custom object to the pipeline
+        $PSObject = [PSCustomObject] @{
+            Version = [RegEx]::Match($($response.ResponseUri.AbsoluteUri), $res.Get.Download.MatchVersion).Captures.Value
+            Date    = ConvertTo-DateTime -DateTime $response.LastModified -Pattern $res.Get.Download.DatePattern
+            Channel = $res.Get.Download.Channel
+            URI     = $response.ResponseUri.AbsoluteUri
         }
+        Write-Output -InputObject $PSObject
     }
 }
