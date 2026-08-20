850dbe23f6e6cf40a9bbb04fb5428f1c0e958f74
Aaron Parker <aaron@stealthpuppy.com>
2026-08-21 09:40:45 +1000

    Add Microsoft Office Deployment Tool app
    Adds `MicrosoftOfficeDeploymentTool` - support for Microsoft Office Deployment Tool and register the app in `.github/microsoft-download-apps.json` so the Microsoft download source is tracked.

---
+++ Apps/Get-MicrosoftOfficeDeploymentTool.ps1
@@ -0,0 +1,24 @@
+function Get-MicrosoftOfficeDeploymentTool {
+    <#
+        .SYNOPSIS
+
+    #>
+    [OutputType([System.Management.Automation.PSObject])]
+    [CmdletBinding(SupportsShouldProcess = $false)]
+    param (
+        [Parameter(Mandatory = $false, Position = 0)]
+        [ValidateNotNull()]
+        [System.Management.Automation.PSObject]
+        $res = (Get-FunctionResource -AppName ("$($MyInvocation.MyCommand)".Split("-"))[1])
+    )
+
+    foreach ($Url in $res.Get.Download.Uri) {
+        [PSCustomObject]@{
+            Version      = $res.Get.Download.Version
+            Date         = ConvertTo-DateTime -DateTime $res.Get.Download.Date -Pattern $res.Get.Download.DatePattern
+            Type         = Get-FileType -File $Url
+            Filename     = (Split-Path -Path $Url -Leaf).Replace('%20', ' ')
+            URI          = $Url -replace ' ', '%20'
+        }
+    }
+}
