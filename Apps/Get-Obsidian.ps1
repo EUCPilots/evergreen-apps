3ac3d6ffc9c431679f98b89ad4f078e81d2915a4
Aaron Parker <aaron@stealthpuppy.com>
2026-09-07 16:07:58 +1000

    Update Obsidian to metadata-driven releases
    Refactors `Get-Obsidian` and its manifest to resolve the latest version from Obsidian’s `desktop-releases.json` metadata, then query GitHub releases by tag (`v<version>`) instead of relying on `releases/latest` #208

--- Apps/Get-Obsidian.ps1
+++ Apps/Get-Obsidian.ps1
@@ -5,7 +5,6 @@ function Get-Obsidian {
 
         .NOTES
             Author: Aaron Parker
-
     #>
     [OutputType([System.Management.Automation.PSObject])]
     [CmdletBinding(SupportsShouldProcess = $false)]
@@ -16,12 +15,24 @@ function Get-Obsidian {
         $res = (Get-FunctionResource -AppName ("$($MyInvocation.MyCommand)".Split("-"))[1])
     )
 
-    # Pass the repo releases API URL and return a formatted object
-    $params = @{
-        Uri          = $res.Get.Uri
-        MatchVersion = $res.Get.MatchVersion
-        Filter       = $res.Get.MatchFileTypes
+    # Get the latest release from the PowerShell metadata
+    try {
+        # Get details from the update feed
+        $updateFeed = Invoke-EvergreenRestMethod -Uri $res.Get.Update.Uri
     }
-    $object = Get-GitHubRepoRelease @params
-    Write-Output -InputObject $object
+    catch {
+        throw "Failed to resolve metadata: $($res.Get.Update.Uri)."
+    }
+
+    # Pass the repo releases API URL and return a formatted object
+    foreach ($Tag in $res.Get.Download.Tags) {
+        Write-Verbose -Message "$($MyInvocation.MyCommand): Query release for tag: $Tag."
+        $params = @{
+            Uri          = "$($res.Get.Download.Uri)$($updateFeed.$Tag)"
+            MatchVersion = $res.Get.Download.MatchVersion
+            Filter       = $res.Get.Download.MatchFileTypes
+        }
+        $object = Get-GitHubRepoRelease @params
+        Write-Output -InputObject $object
+    }
 }
